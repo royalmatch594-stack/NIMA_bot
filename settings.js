64d2bfbe9750377d162e40fcf46733a2491c5100
royalmatch594-stack <royalmatch594@gmail.com>
2025-08-02 12:04:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'NIMA Bot',
-  author: '‎',
-  botName: "NIMA Bot",
-  botOwner: 'NIMA', // Your name
-  ownerNumber: '94726800969', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'ds Bot',
+  author: '‎ds',
+  botName: "ds Bot",
+  botOwner: 'ds', // Your name
+  ownerNumber: '94777712010', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
